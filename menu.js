454e07d5ec6ac67d150e837ad821f29328a82dcb
Ethaniel <ethanielanthony@gmail.com>
2021-01-21 17:31:41 -0700

    wut

--- menu.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(){
-    let navigation = document.querySelector('.menu');
-    let toggle = document.querySelector('.toggle');
-    let links = document.querySelector('.linklist')
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
-    links.classList.toggle('active');
-}
