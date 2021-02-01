b6ad421f936025c541787b1c9c8d2923853fa6ad
Ethaniel Botello <Ebotel200@west-mec.local>
2021-02-01 14:39:30 -0700

    test

--- items/item3/item.js
+++ items/item3/item.js
@@ -1,38 +1,11 @@
-function toggleMenu(){
-    let navigation = document.querySelector('.menu');
-    let toggle = document.querySelector('.toggle');
-    let links = document.querySelector('.linklist');
-    let html = document.querySelector('html');
-    let body = document.querySelector('body');
-    let plop = document.querySelector('.plop');
-    navigation.classList.toggle('active');
-    toggle.classList.toggle('active');
-    links.classList.toggle('active');
-    html.classList.toggle('active');
-    body.classList.toggle('active');
-    plop.classList.toggle('active');
+
+let showFront = function(){
+    itemimg.style.background = "blue";
 }
 
+let showBack = function(){
+    itemimg.style.background = "red";
+}
 
-
-
-let counter = 2
-let counter2 = 8;
-let counter3 = 10;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    document.getElementById('radio' + counter2).checked = true;
-    document.getElementById('radio' + counter3).checked = true;
-    counter++;
-    counter2--;
-    counter3++;
-    if(counter > 4){
-        counter = 1;
-    }
-    if(counter2 < 5){
-        counter2 = 8;
-    }
-    if(counter3 > 12){
-        counter3 = 9;
-    }
-}, 5000);
+front.addEventListener("click", showFront);
+back.addEventListener("click", showBack);
